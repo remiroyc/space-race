18d060bd7e7f95710b4bb0531e55850c7061cbb9
remi <remiroycourt@gmail.com>
2018-06-08 10:47:02 +0200

    Add user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
-import { combineReducers } from "redux"
+import { combineReducers } from 'redux'
 
-import game from "./game"
+import game from './game'
+import user from './user'
 
 export default combineReducers({
-  game: game
+  game: game,
+  user: user
 })
---
+++ src/reducers/user.js
@@ -0,0 +1,12 @@
+const initialState = {
+  distance: 0
+}
+
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default userReducer
