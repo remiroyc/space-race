93157b56eed89c799672ee5731219f3decbdacb0
remi <remiroycourt@gmail.com>
2018-06-08 10:43:34 +0200

    Add fake state

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,4 +1,29 @@
-const initialState = {}
+const initialState = {
+  finished: false,
+  price: 0,
+  ships: {
+    1: {
+      country: 'US',
+      players: 0,
+      distance: 0
+    },
+    2: {
+      country: 'CH',
+      players: 0,
+      distance: 0
+    },
+    3: {
+      country: 'RU',
+      players: 0,
+      distance: 0
+    },
+    4: {
+      country: 'EU',
+      players: 0,
+      distance: 0
+    }
+  }
+}
 
 function gameReducer(state = initialState, action) {
   switch (action.type) {
